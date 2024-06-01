38e51a1d31f293319d0f0733fa67ed843eba22d7
hanulrheem9218 <114366902+hanulrheem9218@users.noreply.github.com>
2024-06-01 21:30:31 +1200

    Introduction to Primer CPP

---
+++ PrimerCpp/PrimerCpp/Source.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib> // what is this
+#include <iostream>
+
+int main() {
+	int x = 0, y = 0;
+	std::cout << "Please enter two numbers";
+	std::cin >> x >> y;
+
+	int sum = x + y;
+	std::cout << sum << std::endl;
+	std::cout << "whats up" << std::endl;
+	return 0;
+}
